8c33ef6bb1a56920a0455a81036b500efadc1404
Aliaksei Krauchanka DEV <aliaksei.krauchanka.dev@gmail.com>
2023-06-10 11:44:56 +0200

    feat: remove admin, add styles for button

--- libs/common-ui/src/lib/button/button.tsx
+++ libs/common-ui/src/lib/button/button.tsx
@@ -5,7 +5,7 @@ export interface ButtonProps {}
 
 export function Button(props: ButtonProps) {
   return (
-    <button>
+    <button class="btn-common">
       Super puper button
     </button>
   );
