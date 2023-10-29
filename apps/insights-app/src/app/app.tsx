48b0435a476f727400af20c8a714ea038b0e2278
akd <aliaksei.krauchanka.dev@gmail.com>
2023-10-29 20:33:14 +0100

    [build] Make local docker work, sync Dockerfiles

--- apps/insights-app/src/app/app.tsx
+++ apps/insights-app/src/app/app.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from 'react';
-import styles from './app.module.scss';
 
 import { Button } from '@lifeis/common-ui';
 import NxWelcome from './nx-welcome';
