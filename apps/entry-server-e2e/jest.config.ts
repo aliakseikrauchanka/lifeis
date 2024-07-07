6ca2ed6498511d18f394d125982916ed9a446994
akd <aliaksei.krauchanka.dev@gmail.com>
2024-07-07 15:11:27 +0200

    Rename node-server -> entry-server

--- apps/entry-server-e2e/jest.config.ts
+++ apps/entry-server-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'node-server-e2e',
+  displayName: 'entry-server-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
--- apps/entry-server/jest.config.ts
+++ apps/entry-server/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'node-server',
+  displayName: 'entry-server',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/node-server',
+  coverageDirectory: '../../coverage/apps/entry-server',
 };
