25a592a6e6478a96d33f44d6c398c04015a5c763
akd <aliaksei.krauchanka.dev@gmail.com>
2024-09-16 09:53:05 +0200

    fix: disable recording by default in Logs page

--- apps/entry-app/src/app/components/log-form/log-form.tsx
+++ apps/entry-app/src/app/components/log-form/log-form.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createLog } from '../../api/logs/logs.api';
 import { SpeechToText } from '../speech-to-text/speech-to-text';
+import { useStorageContext } from '../../contexts/storage.context';
 
 interface ILogFormProps {
   onSubmit: () => void;
 }
 
 export const LogForm = ({ onSubmit }: ILogFormProps) => {
+  const { audioEnabled } = useStorageContext();
   const [message, setMessage] = React.useState('');
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessage(event.target.value);
@@ -26,7 +28,7 @@ export const LogForm = ({ onSubmit }: ILogFormProps) => {
   return (
     <form method="post" onSubmit={handleSubmit}>
       <textarea value={message} name="message" placeholder="Enter your message here" onChange={handleChange} />
-      <SpeechToText onCaption={(caption) => setMessage(caption?.join(' ') || '')} id={'logger'} />
+      {audioEnabled && <SpeechToText onCaption={(caption) => setMessage(caption?.join(' ') || '')} id={'logger'} />}
       <button type="submit">Submit</button>
     </form>
   );
