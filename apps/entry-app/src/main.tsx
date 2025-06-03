fb99431f38b3ac1bd4f5d6c57225a0da72ddf5b8
akd <aliaksei.krauchanka.dev@gmail.com>
2025-06-03 17:08:02 +0200

    Add build for old version of browsers

--- apps/entry-app/src/main.tsx
+++ apps/entry-app/src/main.tsx
@@ -10,7 +10,7 @@ import App from './app/app';
 import { BrowserRouter } from 'react-router-dom';
 import { StorageProvider } from './app/contexts/storage.context';
 
-import.meta.env.VITE_SENTRY_DSN &&
+if (import.meta.env.VITE_SENTRY_DSN) {
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
     integrations: [
@@ -30,6 +30,7 @@ import.meta.env.VITE_SENTRY_DSN &&
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
   });
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const queryClient = new QueryClient();
--- apps/entry-app/vite.config.ts
+++ apps/entry-app/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import legacy from '@vitejs/plugin-legacy';
 // import MillionLint from '@million/lint';
 
 // const wasmPaths = [
@@ -55,6 +56,11 @@ export default defineConfig(
       plugins: [
         /*MillionLint.vite(), */ react(),
         nxViteTsPaths(),
+        legacy({
+          targets: ['Safari >= 12.1'],
+          additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+          modernPolyfills: true,
+        }),
         // viteStaticCopy({
         //   targets: wasmPaths.map((path) => ({
         //     src: path,
